0bd4654d1b99f6701b7f12dfec65dc7c76e8d80b
Wellington Araújo <jwellington427@gmail.com>
2024-05-20 12:23:01 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraAPI/Model/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PrimeiraAPI.Model
+{
+    public interface IEmployeeRepository
+    {
+        void Add(Employee employee);
+
+        List<Employee> Get();
+
+        Employee? Get(int id);
+    }
+}
